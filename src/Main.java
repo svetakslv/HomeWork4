869834af92e31349f5caf56b8f10ed97fecbddda
Светка <sveta.chakrygina@mail.ru>
2022-08-26 22:41:55 +0300

    Задачи №1, №2, №3, №4

--- src/Main.java
+++ src/Main.java
@@ -4,47 +4,53 @@ public class Main {
     public static void main(String[] args) {
 
         //Задача №1
-        {
-            for (int b = 10; b > 0; b--) {
-                System.out.print(b + " ");
-            }
-            System.out.println(" ");
-            int i = 1;
-            while (i <= 10) {
-                System.out.print(i++ + " ");
-
+        {                                                 // Вариант решения от преподавателя:
+            for (int b = 10; b > 0; b--) {                // int currentNum = 0;
+                System.out.print(b + " ");                // while (currentNum < 10) {
+            }                                             // System.out.print(++currentNum + " ");
+            System.out.println(" ");                      // }
+            int i = 1;                                    // System.out.println();
+            while (i <= 10) {                             // for (;currentNum > 0; currentNum--) {
+                System.out.print(i++ + " ");              // System.out.print(currentNum + " ");
+                // }
             }
         }
         //Задача №2
 
-        System.out.println(" ");
-        {
-            for (int fridayReport = 7; fridayReport < 31; fridayReport = fridayReport + 7) {
-                System.out.println("Сегодня пятница " + fridayReport + "-е число. Необходимо подготовить отчет.");
+        System.out.println(" ");                                                                                     //Вариант решения от преподавателя:
+        {                                                                                                            // int firstFriday = 5;
+            for (int fridayReport = 7; fridayReport < 31; fridayReport = fridayReport + 7) {                         // for (int currentFriday = firstFriday; currentFriday <= 31; currentFriday = currentFriday +7) {
+                System.out.println("Сегодня пятница " + fridayReport + "-е число. Необходимо подготовить отчет.");   // System.out.println("Сегодня пятница " + currentFriday + "-е число. Необходимо подготовить отчет.");
             }
-            //Задача №3
-            for (int trajectory = 0; trajectory <= 2100; trajectory = trajectory + 79) {
-                if (trajectory >= 1822) {
-                    System.out.println("Комета пролетала " + trajectory);
-
-
-                }
+            //Задача №3                                                                                              //Вариант решения от преподавателя:
+            for (int trajectory = 0; trajectory <= 2100; trajectory = trajectory + 79) {                             // int currentYear = LocalDate.now().getYear();
+                if (trajectory >= 1822) {                                                                            // int start = currentYear - 200;
+                    System.out.println("Комета пролетала в " + trajectory + " году.");                               // int end = currentYear + 100;
+                    // for (int year = start; year <= end; year++) {
+                    // if (year % 79 == 0) {
+                }                                                                                                    // System.out.println(year);
             }
         }
         //*Задача №4
         for (int i = 1; i < 30; i++) {
-            if (i / 3 == 0) {
-                System.out.println("ping");
-                if (i / 5 == 0) {
-                    System.out.println("pong");
+            if (i % 3 == 0) {
+                System.out.println(i + ":" + " ping");
+            } else {
+                if (i % 5 == 0) {
+                    System.out.println(i + ":" + " pong");
                 } else {
-                    if (i / 3 == 0 && i / 5 == 0) {
-                        System.out.println("ping pong");
+                    if (i % 3 == 0 && i % 5 == 0) {
+                        System.out.println(i + ":" + " ping pong");
+                    } else {
+                        if (i % 3 != 0 && i % 5 != 0) ;
+                        System.out.println(i + ":" + " ");
                     }
                 }
             }
         }
     }
+        //*Задача №5
+
 }
 
 
