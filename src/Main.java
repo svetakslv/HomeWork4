30121b2b61521d5cd146caf2c1e2a81346050018
Светка <sveta.chakrygina@mail.ru>
2022-08-26 20:29:20 +0300

    Задачи №1, №2, №3

--- src/Main.java
+++ src/Main.java
@@ -31,7 +31,22 @@ public class Main {
                 }
             }
         }
+        //*Задача №4
+        for (int i = 1; i < 30; i++) {
+            if (i / 3 == 0) {
+                System.out.println("ping");
+                if (i / 5 == 0) {
+                    System.out.println("pong");
+                } else {
+                    if (i / 3 == 0 && i / 5 == 0) {
+                        System.out.println("ping pong");
+                    }
+                }
+            }
+        }
     }
 }
+
+
 
 
